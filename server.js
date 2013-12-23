690754a61528be4c6d2a340a7d108328c72d5dd6
Fravic <fravicf@gmail.com>
2013-12-22 22:32:50 -0500

    ADD fleshed out dropbox calls (untested)

--- server.js
+++ server.js
@@ -1,32 +1,70 @@
 var express = require('express');
 var dropbox = require('./lib/dropbox');
 
+var USER_DATA_FILENAME = ".user_data";
+
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(req, res) {
-    var path, params;
+    var path, params, userData, list = [], itemsOutstanding;
+
     path = process.argv[1];
     if (!path) {
         console.error("Must supply Dropbox path to serve!");
+        res.send(500);
     }
     params = {list: true};
 
-    // List files in path
-    dropbox.metadata("dropbox", path, params, function(data) {
+    function onReceivedUserData(data) {
+        if (data != null) {
+            userData = data;
+            dropbox.metadata("dropbox", path, params, onReceivedFileList);
+        } else {
+            res.send(500);
+        }
+    }
+
+    function onReceivedFileList(data) {
         if (data != null) {
             if (data["contents"]) {
+                itemsOutstanding = data["contents"].length;
                 data["contents"].forEach(function(file) {
+                    var itemPath;
                     if (!data["contents"]["is_dir"]) {
-                        // Append user data to return data
-                        // Append media url to return data
+                        itemPath = data["contents"]["path"];
+                        dropbox.media(itemPath, {}, function(data) {
+                            onReceivedMediaURL(itemPath, data);
+                        });
                     }
                 });
             } else {
                 console.error("Dropbox path provided is not a directory.");
+                res.send(500);
             }
         }
-    });
+    }
+
+    function onReceivedMediaURL(itemPath, data) {
+        itemsOutstanding -= 1;
+
+        if (data != null) {
+            var item = {};
+            if (userData && userData[itemPath]) {
+                item["userData"] = userData;
+            }
+            item["url"] = data["url"];
+            list += item;
+        } else {
+            console.warn("Warning: Invalid item path " + itemPath);
+        }
+
+        if (itemsOutstanding == 0) {
+            res.status(200).send(items);
+        }
+    }
+
+    dropbox.files(path + USER_DATA_FILENAME, onReceivedUserData);
 });
 
 app.get('/data', function(req, res) {
